e9f85832e40840ef4308adfa12de4becc80e9f77
parentda <danielparent21@gmail.com>
2021-06-26 17:39:46 -0400

    Changed some stuff

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,8 @@
-const gameOptions = ["Rock", "Paper", "Scissors"];
+const gameOptions = ["rock", "paper", "scissors"];
 let score;
 
 let roundCounter = 0;
 let gameEndpoint = 0;
-let gameInProgress = false;
 
 const startGameButton = document.querySelector("#startGame");
 const playOptions = document.querySelectorAll(".playOptions > button");
@@ -20,7 +19,6 @@ function computerPlay() {
 function startGame(event) {
   score = [0, 0, 0];
   roundCounter = 0;
-  gameInProgress = true;
   result.textContent = "";
   finalScore.textContent = "";
   // console.log(event);
@@ -36,6 +34,7 @@ function startGame(event) {
   gameEndpoint = inputRounds.value;
 
   trackRounds.textContent = roundCounter;
+  startGameButton.textContent = "Restart Game";
 }
 
 function showFinalScore() {
@@ -47,17 +46,17 @@ function showFinalScore() {
   }
 
   if (score[0] === score[2]) {
-    finalScore.textContent = `After ${roundCounter} ${roundVerbiage}, it's a tie!`;
+    finalScore.textContent = `after ${roundCounter} ${roundVerbiage}, it's a tie!`;
   } else if (score[0] > score[2]) {
-    finalScore.textContent = `After ${roundCounter} ${roundVerbiage}, you win the game ${score[0]}-${score[2]}!`;
+    finalScore.textContent = `after ${roundCounter} ${roundVerbiage}, you win the game ${score[0]}-${score[2]}!`;
   } else {
-    finalScore.textContent = `After ${roundCounter} ${roundVerbiage}, you lose the game ${score[2]}-${score[0]}!`;
+    finalScore.textContent = `after ${roundCounter} ${roundVerbiage}, you lose the game ${score[2]}-${score[0]}!`;
   }
 }
 
 function playRound(event) {
   const computerSelection = computerPlay();
-  const playerSelection = event.target.innerText;
+  const playerSelection = event.target.id;
   console.log(event);
 
   const indexDiff =
@@ -66,15 +65,15 @@ function playRound(event) {
 
   if (indexDiff === 0) {
     score[1] += 1;
-    result.textContent = `It's a Tie!`;
+    result.textContent = `it's a tie!`;
     scoreOptions[1].textContent = score[1];
   } else if (indexDiff === 1 || indexDiff === -2) {
     score[0] += 1;
-    result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    result.textContent = `you win! ${playerSelection} beats ${computerSelection}`;
     scoreOptions[0].textContent = score[0];
   } else {
     score[2] += 1;
-    result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    result.textContent = `you lose! ${computerSelection} beats ${playerSelection}`;
     scoreOptions[2].textContent = score[2];
   }
 
